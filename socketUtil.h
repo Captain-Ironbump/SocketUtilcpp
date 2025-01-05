171b01493ad7ccf82b75e02d9bbd5e3fd62b9e17
Lukas Linde <lindelu@tbdir.net>
2025-01-06 00:09:17 +0100

    macOS support

--- socketUtil.h
+++ socketUtil.h
@@ -3,10 +3,15 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <malloc.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
+#ifdef __APPLE__
+    #include <stdlib.h>
+#elif defined(__linux__)
+    #include <malloc.h>
+#endif
 
 struct sockaddr_in* createIPv4SockAddr(const char* ip, int port);
 int createTCPIPv4Socket();
