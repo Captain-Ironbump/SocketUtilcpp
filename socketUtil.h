fd21c415f8d49904539058aab71f0b83aa680260
Lukas Linde <linde.lukas.99@gmail.com>
2025-01-14 23:43:05 +0100

    update windows support

--- socketUtil.h
+++ socketUtil.h
@@ -3,9 +3,27 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+
+#ifdef _WIN32
+    #include <winsock2.h>  // Include Windows-specific networking headers
+    #pragma comment(lib, "ws2_32.lib")  // Link against Winsock library
+
+    // Initialize Winsock (this is needed only for Windows)
+    #define INIT_WINSOCK() \
+        do { \
+            WSADATA wsaData; \
+            if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) { \
+                fprintf(stderr, "WSAStartup failed\n"); \
+                return NULL; \
+            } \
+        } while(0)
+    #define CLEANUP_WINSOCK() WSACleanup()
+
+#else
+    #include <sys/socket.h>  // POSIX socket headers for Linux/macOS
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+#endif
 
 #ifdef __APPLE__
     #include <stdlib.h>
